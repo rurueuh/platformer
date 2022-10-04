39962a45063cb7edb60b15a684f101049d8f2e8b
Maximilien Vanstaevel <maximilien.vanstaevel@epitech.eu>
2022-10-04 12:18:30 +0200

    remove useless file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ruru/Desktop/epitech/perso/gravitySpacial
-# Build directory: /home/ruru/Desktop/epitech/perso/gravitySpacial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
